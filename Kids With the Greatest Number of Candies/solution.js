10ed4a0a856da820aa52d09f20134fb0425c4ba8
Ethan Shealey <ethan.shealey@gmail.com>
2022-02-06 20:27:29 -0500

    Delete solution.js

--- Kids With the Greatest Number of Candies/solution.js
+++
@@ -1,4 +0,0 @@
-const kidsWithCandies = (candies, extraCandies, res = []) => {
-    candies.forEach((count, i) => Math.max(...candies, count + extraCandies) === count + extraCandies ? res.push(1) : res.push(0))
-    return res
-}
